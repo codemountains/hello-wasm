22780a8ed9e247f9732d19b182f58a190644d17b
codemountains <4kz12zz@gmail.com>
2022-10-15 18:02:17 +0900

    :bug: fix build command

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cargo build --target wasm32-unknown-unknown --release
-wasm-bindgen target/wasm32-unknown-unknown/release/ulid-wasm.wasm --out-dir ./pkg --target no-modules
+wasm-bindgen target/wasm32-unknown-unknown/release/ulid_wasm.wasm --out-dir ./pkg --target no-modules
